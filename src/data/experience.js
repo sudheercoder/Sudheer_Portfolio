2d675b1e4464781a2ec1b69019d568f16db51bf2
Sudheer Pal <spal57304@gmail.com>
2026-07-02 19:43:44 +0530

    experience

--- src/data/experience.js
+++ src/data/experience.js
@@ -3,7 +3,7 @@ export const experienceData = [
     id: 1,
     company: 'Digicoders Technologies',
     role: 'MERN Stack Developer',
-    duration: 'Jan-2026 - Present ',
+    duration: 'July-2025 - Present ',
     year: '2026',
     location: 'Lucknow, Uttar Pradesh',
     type: 'Developer',
